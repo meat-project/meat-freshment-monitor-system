7dbb32fa2129c4e735810740a7ae5b0ae28bc67e
CollieIsCute <qetup1988@gmail.com>
2021-10-19 04:58:13 +0800

    rename realtime sensor output file

--- codes/python_/serial_sensor.py
+++ codes/python_/serial_sensor.py
@@ -7,5 +7,5 @@ ser=serial.Serial('/dev/ttyACM0', 9600)
 while True:
 	serialdata = ser.readline()
 	print(serialdata.decode('utf-8'), end='')
-	with open('../../result/testout.txt', 'a') as f:
+	with open('../../result/realtime_sensor_out.txt', 'a') as f:
 		f.write(serialdata.decode('utf-8'))
