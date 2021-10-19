139efa0eec4b190d91c8fa4847acde03b1fd1b7d
LMT5 <81086325+LMT5@users.noreply.github.com>
2021-10-19 14:27:08 +0800

    Python code
    儲存模型，輸出模型，上傳模型，使用模型

--- codes/python_/classify_test.py
+++ codes/python_/classify_test.py
@@ -1,9 +1,11 @@
 #相關函式庫
 import pandas as pd 
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import pickle
 
 df = pd.read_excel('../../result/data.xlsm')
 sdf = pd.DataFrame(df)
@@ -14,25 +16,42 @@ X = sdf.drop(['label'], axis = 1);
 y = sdf['label']
 
 print("\n ----------  Adaboost Classifier  ----------\n")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state = 5)
+abc = AdaBoostClassifier(n_estimators = 100, learning_rate = 1)
+model = abc.fit(X_train, y_train)
+y_pred = abc.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("y_test:",list(y_test))
+print("y_pred:",list(y_pred),"\n")
 
-for i in range(20):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state = i)
-    print("seed:",i)
-    abc = AdaBoostClassifier(n_estimators = 100, learning_rate = 1)
-    model = abc.fit(X_train, y_train)
-    y_pred = abc.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    print("y_test:",list(y_test))
-    print("y_pred:",list(y_pred),"\n")
+##save module
+with open('AdaboostClassifier.pickle','wb') as f:
+     pickle.dump(abc, f)
     
 print("\n ----------  Bayesian Classifier  ----------\n")
 
-for i in range(20):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state = i)
-    print("seed:",i)
-    abc = GaussianNB()
-    abc.fit(X_train,y_train)
-    y_pred = abc.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    print("y_test:",list(y_test))
-    print("y_pred:",list(y_pred),"\n")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state = 18)
+abc = GaussianNB()
+abc.fit(X_train,y_train)
+y_pred = abc.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("y_test:",list(y_test))
+print("y_pred:",list(y_pred),"\n")
+
+##save module
+with open('BayesianClassifier.pickle','wb') as f:
+     pickle.dump(abc, f)
+
+print("\n ----------  KNN  ----------\n")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3,random_state = 8)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train,y_train)
+y_pred = neigh.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("y_test:",list(y_test))
+print("y_pred:",list(y_pred))
+
+##save module
+with open('KNN.pickle','wb') as f:
+     pickle.dump(abc, f)
--- codes/python_/serial_sensor.py
+++ codes/python_/serial_sensor.py
@@ -1,6 +1,7 @@
 import serial
 import string
 import time
+import pickle
 #opening serial port
 ser=serial.Serial('/dev/ttyACM0', 9600)
 
@@ -9,3 +10,16 @@ while True:
 	print(serialdata.decode('utf-8'), end='')
 	with open('../../result/realtime_sensor_out.txt', 'a') as f:
 		f.write(serialdata.decode('utf-8'))
+
+#AdaboostClassifier
+"""pickle_in = open('AdaboostClassifier.pickle','rb')
+clf = pickle.load(pickle_in)
+predict_result = clf.predict(X_predict)"""
+#BayesianClassifier.pickle
+"""pickle_in = open('BayesianClassifier.pickle','rb')
+clf = pickle.load(pickle_in)
+predict_result = clf.predict(X_predict)"""
+#KNN
+"""pickle_in = open('KNN.pickle','rb')
+clf = pickle.load(pickle_in)
+predict_result = clf.predict(X_predict)"""
