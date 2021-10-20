f558bcbd46c064c4b785093950b1fb9e9e225a12
LMT5 <81086325+LMT5@users.noreply.github.com>
2021-10-20 16:29:21 +0800

    demo
    demo version

--- codes/python_/serial_sensor.py
+++ codes/python_/serial_sensor.py
@@ -10,18 +10,17 @@ ser=serial.Serial('/dev/ttyACM0', 9600)
 def getPredictX():
 	serialdata = ''
 	ret = {}
-	print("fuck"+repr(serialdata));
+	print("fuck"+repr(serialdata))
 	while serialdata == '' or serialdata.split()[0] != 'H2S:':
 		serialdata = ser.readline().decode('utf-8')
-		
-		#print('before split:' + serialdata)
-	#print(serialdata.split()[1])
+		print('before split:' + serialdata)
+	print(serialdata.split()[1])
 	ret['H2S'] = float(serialdata.split()[1])
 	serialdata = ser.readline().decode('utf-8')
-	#print(serialdata.split()[1])
+	print(serialdata.split()[1])
 	ret['NH3'] = float(serialdata.split()[1])
 	serialdata = ser.readline().decode('utf-8')
-	#print(serialdata.split()[1])
+	print(serialdata.split()[1])
 	ret['CO2'] = float(serialdata.split()[1])
 	return ret
 	
@@ -36,17 +35,17 @@ while True:
 	pickle_in = open('AdaboostClassifier.pickle','rb')
 	clf = pickle.load(pickle_in)
 	predict_result = clf.predict(X_predict)
-	print("adaboost: ", X_predict)
+	print("adaboost: \n", predict_result)
 	#BayesianClassifier.pickle
 	pickle_in = open('BayesianClassifier.pickle','rb')
 	clf = pickle.load(pickle_in)
 	predict_result = clf.predict(X_predict)
-	print("BayesianClassifier: ", X_predict)
+	print("BayesianClassifier: \n", predict_result)
 	#KNN
 	pickle_in = open('KNN.pickle','rb')
 	clf = pickle.load(pickle_in)
 	predict_result = clf.predict(X_predict)
-	print("KNN: ", X_predict)
+	print("KNN: \n", predict_result)
 	#decisiontree
 	"""pickle_in = open('DecisionTreeClassifier.pickle','rb')
 	clf = pickle.load(pickle_in)
