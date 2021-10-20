1447a3ae59844513f9dcbcda2fd7153f01e23e3f
LMT5 <81086325+LMT5@users.noreply.github.com>
2021-10-20 16:25:54 +0800

    trash

--- codes/python_/serial_sensor.py
+++ codes/python_/serial_sensor.py
@@ -2,28 +2,52 @@ import serial
 import string
 import time
 import pickle
+import pandas as pd
+
 #opening serial port
 ser=serial.Serial('/dev/ttyACM0', 9600)
 
-while True:
-	serialdata = ser.readline()
-	print(serialdata.decode('utf-8'), end='')
-	with open('../../result/realtime_sensor_out.txt', 'a') as f:
-		f.write(serialdata.decode('utf-8'))
+def getPredictX():
+	serialdata = ''
+	ret = {}
+	print("fuck"+repr(serialdata))
+	while serialdata == '' or serialdata.split()[0] != 'H2S:':
+		serialdata = ser.readline().decode('utf-8')
+		print('before split:' + serialdata)
+	print(serialdata.split()[1])
+	ret['H2S'] = float(serialdata.split()[1])
+	serialdata = ser.readline().decode('utf-8')
+	print(serialdata.split()[1])
+	ret['NH3'] = float(serialdata.split()[1])
+	serialdata = ser.readline().decode('utf-8')
+	print(serialdata.split()[1])
+	ret['CO2'] = float(serialdata.split()[1])
+	return ret
+	
 
-#AdaboostClassifier
-"""pickle_in = open('AdaboostClassifier.pickle','rb')
-clf = pickle.load(pickle_in)
-predict_result = clf.predict(X_predict)"""
-#BayesianClassifier.pickle
-"""pickle_in = open('BayesianClassifier.pickle','rb')
-clf = pickle.load(pickle_in)
-predict_result = clf.predict(X_predict)"""
-#KNN
-"""pickle_in = open('KNN.pickle','rb')
-clf = pickle.load(pickle_in)
-predict_result = clf.predict(X_predict)"""
-#decisiontree
-"""pickle_in = open('DecisionTreeClassifier.pickle','rb')
-clf = pickle.load(pickle_in)
-predict_result = clf.predict(X_predict)"""
+while True:
+	dic = getPredictX()
+	#print(dic)
+	X_predict = pd.DataFrame([dic])
+	#print(X_predict)
+
+	#AdaboostClassifier
+	pickle_in = open('AdaboostClassifier.pickle','rb')
+	clf = pickle.load(pickle_in)
+	predict_result = clf.predict(X_predict)
+	print("adaboost: \n", predict_result)
+	#BayesianClassifier.pickle
+	pickle_in = open('BayesianClassifier.pickle','rb')
+	clf = pickle.load(pickle_in)
+	predict_result = clf.predict(X_predict)
+	print("BayesianClassifier: \n", predict_result)
+	#KNN
+	pickle_in = open('KNN.pickle','rb')
+	clf = pickle.load(pickle_in)
+	predict_result = clf.predict(X_predict)
+	print("KNN: \n", predict_result)
+	#decisiontree
+	"""pickle_in = open('DecisionTreeClassifier.pickle','rb')
+	clf = pickle.load(pickle_in)
+	predict_result = clf.predict(X_predict)
+	print("decisiontree: ", X_predict)"""
