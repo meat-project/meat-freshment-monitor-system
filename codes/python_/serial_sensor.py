f5f9c644e60114712fe77ba2adb19bf7cddb16df
CollieIsCute <qetup1988@gmail.com>
2021-10-21 19:53:53 +0800

    make serial_senser show up easier to read

--- codes/python_/serial_sensor.py
+++ codes/python_/serial_sensor.py
@@ -10,17 +10,15 @@ ser=serial.Serial('/dev/ttyACM0', 9600)
 def getPredictX():
 	serialdata = ''
 	ret = {}
-	print("fuck"+repr(serialdata))
-	while serialdata == '' or serialdata.split()[0] != 'H2S:':
+	while serialdata.split() == [] or serialdata.split()[0] != 'H2S:': # sometimes there's a bug "out of range...". Add `print('',end='')` can fix it.
 		serialdata = ser.readline().decode('utf-8')
-		print('before split:' + serialdata)
-	print(serialdata.split()[1])
+	print('H2S: ', serialdata.split()[1], ' ppm')
 	ret['H2S'] = float(serialdata.split()[1])
 	serialdata = ser.readline().decode('utf-8')
-	print(serialdata.split()[1])
+	print('NH3: ', serialdata.split()[1], ' ppm')
 	ret['NH3'] = float(serialdata.split()[1])
 	serialdata = ser.readline().decode('utf-8')
-	print(serialdata.split()[1])
+	print('CO2: ', serialdata.split()[1], ' ppm')
 	ret['CO2'] = float(serialdata.split()[1])
 	return ret
 	
@@ -47,7 +45,7 @@ while True:
 	predict_result = clf.predict(X_predict)
 	print("KNN: \n", predict_result)
 	#decisiontree
-	"""pickle_in = open('DecisionTreeClassifier.pickle','rb')
+	pickle_in = open('DecisionTreeClassifier.pickle','rb')
 	clf = pickle.load(pickle_in)
 	predict_result = clf.predict(X_predict)
-	print("decisiontree: ", X_predict)"""
+	print("decisiontree: \n", predict_result)
